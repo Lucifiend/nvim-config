40fbd3139a2cafcf138619af900f02647a317800
Lucifiend <190273418+Lucifiend@users.noreply.github.com>
2026-03-26 11:50:13 +0000

    add: oil.nvim

--- lua/lucifiend/keymap.lua
+++ lua/lucifiend/keymap.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader= " "
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", "<CMD>Oil<CR>")
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
